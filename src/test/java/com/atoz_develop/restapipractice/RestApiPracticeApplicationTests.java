de6571f2486de767e7424949d44c0c10fb86aa34
Yoon <Yoon@Yoonui-MacBook-Pro.local>
2020-04-03 22:29:05 +0900

    Initial commit

---
+++ src/test/java/com/atoz_develop/restapipractice/RestApiPracticeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atoz_develop.restapipractice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestApiPracticeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
